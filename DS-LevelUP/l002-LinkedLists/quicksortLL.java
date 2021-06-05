e7f6ca6dec5b669a5fd6fb0d36a0b8686757ddd8
Ashutosh Sidhar <ashutoshsidhar1999@gmail.com>
2021-06-05 17:34:25 +0530

    update

--- DS-LevelUP/l002-LinkedLists/quicksortLL.java
+++ DS-LevelUP/l002-LinkedLists/quicksortLL.java
@@ -1,5 +1,5 @@
 public class quicksortLL{
-    public static class ListNode {
+    public static  class ListNode {
         int val = 0;
         ListNode next = null;
 
@@ -10,7 +10,7 @@ public class quicksortLL{
 
     Random rand = new Random();
 
-    public int length(ListNode node) {
+    public static int length(ListNode node) {
         if (node == null)
             return 0;
 
@@ -24,7 +24,7 @@ public class quicksortLL{
         return len;
     }
 
-    public ListNode[] segregate(ListNode head, int pivotIdx) {
+    public static ListNode[] segregate(ListNode head, int pivotIdx) {
         ListNode small = new ListNode(-1);
         ListNode large = new ListNode(-1);
         ListNode pivotNode = head, sp = small, lp = large, curr = head;
@@ -52,7 +52,7 @@ public class quicksortLL{
         return new ListNode[] { small.next, pivotNode, large.next };
     }
 
-    public ListNode[] mergeElements(ListNode[] left, ListNode pivotNode, ListNode[] right) {
+    public static ListNode[] mergeElements(ListNode[] left, ListNode pivotNode, ListNode[] right) {
         ListNode head = null;
         ListNode tail = null;
         if (left[0] != null && right[0] != null) {
@@ -75,7 +75,7 @@ public class quicksortLL{
         return new ListNode[] { head, tail };
     }
 
-    public ListNode[] quickSort(ListNode head) {
+    public static ListNode[] quickSort(ListNode head) {
         if (head == null || head.next == null)
             return new ListNode[] { head, head };
 
@@ -89,8 +89,97 @@ public class quicksortLL{
         return mergeElements(left, segregatedElements[1], right);
     }
 
-    public ListNode sortList(ListNode head) {
+    public static ListNode sortList(ListNode head) {
         return quickSort(head)[0];
     }
 
 }
+
+
+
+/*
+    // Portal sol
+
+  public static int length(ListNode node) {
+    if (node == null)
+      return 0;
+
+    ListNode curr = node;
+    int len = 0;
+    while (curr != null) {
+      len++;
+      curr = curr.next;
+    }
+
+    return len;
+  }
+
+  public static ListNode[] segregate(ListNode head, int pivotIdx) {
+    ListNode small = new ListNode(-1);
+    ListNode large = new ListNode(-1);
+    ListNode pivotNode = head, sp = small, lp = large, curr = head;
+    while (pivotIdx-- > 0)
+      pivotNode = pivotNode.next;
+
+    while (curr != null) {
+      if (curr != pivotNode) {
+        if (curr.val <= pivotNode.val) {
+          sp.next = curr;
+          sp = sp.next;
+        } else {
+          lp.next = curr;
+          lp = lp.next;
+        }
+      }
+
+      curr = curr.next;
+    }
+
+    pivotNode.next = null;
+    sp.next = null;
+    lp.next = null;
+
+    return new ListNode[] { small.next, pivotNode, large.next };
+  }
+
+  public static ListNode[] mergeElements(ListNode[] left, ListNode pivotNode, ListNode[] right) {
+    ListNode head = null;
+    ListNode tail = null;
+    if (left[0] != null && right[0] != null) {
+      head = left[0];
+      tail = right[1];
+      left[1].next = pivotNode;
+      pivotNode.next = right[0];
+    } else if (left[0] != null) {
+      head = left[0];
+      tail = pivotNode;
+      left[1].next = pivotNode;
+    } else if (right[0] != null) {
+      head = pivotNode;
+      tail = right[1];
+      pivotNode.next = right[0];
+    } else {
+      head = tail = pivotNode;
+    }
+
+    return new ListNode[] { head, tail };
+  }
+
+  public static ListNode[] quickSort_(ListNode head) {
+    if (head == null || head.next == null)
+      return new ListNode[] { head, head };
+
+    int len = length(head);
+    int pivotIdx = len / 2;
+    ListNode[] segregatedElements = segregate(head, pivotIdx);
+
+    ListNode[] left = quickSort_(segregatedElements[0]);
+    ListNode[] right = quickSort_(segregatedElements[2]);
+
+    return mergeElements(left, segregatedElements[1], right);
+  }
+
+  public static ListNode quickSort(ListNode head) {
+    return quickSort_(head)[0];
+  }
+*/ 
